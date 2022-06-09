2d4855d690879196e1a450d9c9f6f2cb9404b99d
Adelina <tapesadelina@gmail.com>
2022-06-10 00:41:55 +0300

    semi-final header2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import './Header.css'
+import Logo from "./img/menu.png"
 
 export default function Header(props) {
     return (
         <div className="header">
-            <i className="fas fa-bars" id="menu"></i>
+            <img src={Logo} className="image"/>
             <div className="hd__apis">
                 <p className="hd__location">location</p>
                 <p className="hd__date">date</p>
