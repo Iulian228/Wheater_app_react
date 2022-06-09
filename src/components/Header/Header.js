9025bc1932cdae2846117e6c09f867b5d60b7582
Adelina <tapesadelina@gmail.com>
2022-06-09 20:45:25 +0300

    the beginning of header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,9 @@ import './Header.css'
 export default function Header(props) {
     return (
         <div className="header">
-            <div className="hd__icon"></div>
+            <i className="fas fa-bars" id="menu"></i>
             <div className="hd__location"></div>
+            <div className="hd__icon"></div>
             <div className="hd__date"></div>
         </div>
     )
