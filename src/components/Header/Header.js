ea183c327c284220c070a78537b57e7f1b8e0e79
Stefan <stefanguzun2005@gmail.com>
2022-06-15 00:39:10 +0300

    we made the sidebar interactivewith the header's menu icon(fade__in)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import './Header.css'
 import Logo from "./img/menu.png"
+import Sidebar from '../Sidebar/Sidebar'
 
 export default function Header(props) {
+    const afterClick = () => {
+        const menu = document.querySelector(".MenuMobil")
+        menu.classList.toggle("fade__in")
+    }
     return (
         <div className="header">
-            <img src={Logo} className="image"/>
+            <img onClick={afterClick} src={Logo} className="image"/>
             <div className="hd__apis">
                 <p className="hd__location">location</p>
                 <p className="hd__date">date</p>
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -3,12 +3,15 @@ import Header from "../Header/Header";
 import Forecast from '../Forecast/Forecast';
 import Sidebar from "../Sidebar/Sidebar";
 
+
 export default function MainPage(props) {
     return (
-        <div className="main sidebar-move">
-            <Header/>
+        <div className="main__parent">
             <Sidebar/>
+        <div className="main">
+            <Header/>
             <Forecast position={props.position}/>
+        </div>
         </div>
     )
 }
